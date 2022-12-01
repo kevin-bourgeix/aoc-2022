cefdfe410468e4ce78aabafea3ee3f179088eff6
Kevin Bourgeix <90198057+kevin-bourgeix@users.noreply.github.com>
2022-12-01 09:51:16 +0100

    ✨ day1 (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-// TODO : Remove this cfg when using the module. This is for clippy to be ok.
-#[cfg(test)]
+mod exercises;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    // Day 1.1
+    let day1_1 = exercises::day1::day_1_1("./src/files/day1.1");
+    println!("Day 1.1 : {day1_1}");
+
+    // Day 1.2
+    let day1_2 = exercises::day1::day_1_2("./src/files/day1.1");
+    println!("Day 1.2 : {day1_2}");
 }
