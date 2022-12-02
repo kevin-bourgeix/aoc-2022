00b004f3bb320daac01edad320c724c8d958bef2
Kevin Bourgeix <90198057+kevin-bourgeix@users.noreply.github.com>
2022-12-02 11:52:41 +0100

    ✨ day2 (#3)

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,12 @@ fn main() {
     // Day 1.2
     let day1_2 = exercises::day1::day_1_2("./src/files/day1.1");
     println!("Day 1.2 : {day1_2}");
+
+    // Day 2.1
+    let day2_1 = exercises::day2::day_2_1("./src/files/day2");
+    println!("Day 2.1 : {day2_1}");
+
+    // Day 2.2
+    let day2_2 = exercises::day2::day_2_2("./src/files/day2");
+    println!("Day 2.2 : {day2_2}");
 }
