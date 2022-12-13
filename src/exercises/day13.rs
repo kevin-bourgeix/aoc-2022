1f9596ae5051028988c1b33fd08d37e5fed47865
Kevin Bourgeix <kevin.bourgeix@gokepler.fr>
2022-12-13 19:01:02 +0100

    ✨ day13

---
+++ src/exercises/day13.rs
@@ -0,0 +1,244 @@
+use std::cmp::Ordering;
+
+use crate::parser::parse_aoc_file;
+
+#[derive(Debug, Clone)]
+enum Packet {
+    Int(i32),
+    Array(Vec<Packet>),
+}
+
+// it would be a good exercise to implement the Iterator trait for Packet and would simplify the code
+
+impl Packet {
+    fn from_vec(v: Vec<Packet>) -> Self {
+        Self::Array(v)
+    }
+    fn slice(&self, i: usize, j: usize) -> Packet {
+        match self {
+            Self::Int(_) => panic!("Cannot slice an integer"),
+            Self::Array(a) => Self::Array(a[i..j].to_vec()),
+        }
+    }
+    fn first(&self) -> Packet {
+        match self {
+            Self::Int(_) => panic!("Cannot flatten an integer"),
+            Self::Array(a) => a.first().unwrap_or(&Self::Array(vec![])).clone(),
+        }
+    }
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Int(l0), Self::Int(r0)) => l0 == r0,
+            (Self::Array(l0), Self::Array(r0)) => {
+                l0.len() == r0.len() && l0.iter().enumerate().all(|(i, l)| *l == r0[i])
+            }
+            (Self::Int(l0), Self::Array(r0)) => r0 == &[Self::Int(*l0)],
+            (Self::Array(l0), Self::Int(r0)) => l0 == &[Self::Int(*r0)],
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Packet::Int(i), Packet::Int(j)) => i.partial_cmp(j),
+            (Packet::Array(a), Packet::Array(b)) => {
+                if a.is_empty() && b.is_empty() {
+                    return Some(Ordering::Equal);
+                }
+                let Some(a_item) = a.first() else {
+                    return Some(Ordering::Less);
+                };
+                let Some(b_item) = b.first() else {
+                    return Some(Ordering::Greater);
+                };
+                match a_item.partial_cmp(b_item) {
+                    Some(Ordering::Equal) => {
+                        self.slice(1, a.len()).partial_cmp(&other.slice(1, b.len()))
+                    }
+                    Some(Ordering::Less) => Some(Ordering::Less),
+                    Some(Ordering::Greater) => Some(Ordering::Greater),
+                    None => None,
+                }
+            }
+            (Packet::Array(a), Packet::Int(i)) => a.partial_cmp(&vec![Packet::Int(*i)]),
+            (Packet::Int(i), Packet::Array(a)) => vec![Packet::Int(*i)].partial_cmp(a),
+        }
+    }
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // safe unwrap because we theoretically never return None (as i32.partial_cmp(i32) never does)
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl Eq for Packet {}
+
+fn parse_file(filename: &str) -> Vec<String> {
+    parse_aoc_file(filename, Some("\n\n"))
+}
+
+fn parse_line(line: String) -> Vec<Packet> {
+    let mut curr: Vec<Packet> = Vec::new();
+    let mut stack: Vec<Vec<Packet>> = Vec::new();
+    let mut current_number: String = String::new();
+    let mut col = 1;
+
+    for c in line.chars() {
+        match c {
+            '[' => {
+                stack.push(curr);
+                curr = Vec::new();
+            }
+            ']' => {
+                let Some(mut last) = stack.pop() else {
+                    panic!("Unbalanced brackets at col {col}");
+                };
+                if !current_number.is_empty() {
+                    let Ok(number) = current_number.parse() else {
+                        panic!("Invalid number {current_number} read after ']' at col {col}");
+                    };
+                    curr.push(Packet::Int(number));
+                    current_number = String::new();
+                }
+                last.push(Packet::Array(curr));
+                curr = last;
+            }
+            ',' => {
+                if !current_number.is_empty() {
+                    let Ok(number) = current_number.parse() else {
+                        panic!("Invalid number {current_number} read after ',' at col {col}");
+                    };
+                    curr.push(Packet::Int(number));
+                    current_number = String::new();
+                }
+            }
+            _ => {
+                current_number.push(c);
+            }
+        }
+        col += 1;
+    }
+
+    curr
+}
+
+fn is_right_order(pair: &str) -> bool {
+    let lines = pair.split('\n');
+    lines.map(|line| parse_line(line.to_string())).is_sorted()
+}
+
+pub fn day_13_1(filename: &str) -> usize {
+    let pairs = parse_file(filename);
+    pairs.iter().enumerate().fold(0, |acc, (i, pair)| {
+        if is_right_order(pair) {
+            acc + i + 1
+        } else {
+            acc
+        }
+    })
+}
+
+pub fn day_13_2(filename: &str) -> i32 {
+    let mut packets: Vec<Packet> = parse_aoc_file(filename, None)
+        .iter()
+        .filter(|l| !l.is_empty())
+        // not ideal as we put everything in an array to take it out after, but i don't want to debug the parser
+        .map(|l| Packet::from_vec(parse_line(l.clone())).first())
+        .collect();
+    packets.sort();
+    let (div_x, div_y) = packets
+        .iter()
+        .enumerate()
+        .fold((-1, -1), |(x, y), (i, packet)| {
+            if let Packet::Array(a) = packet && a.len() == 1 && let Packet::Array(b) = a.first().unwrap() && b.len() == 1 {
+                if let Packet::Int(value) = b.first().unwrap() {
+                    if *value == 2 {
+                        return (i as i32 + 1, y)
+                    } else if *value == 6  {
+                        return (x, i as i32 + 1)
+                    }
+                }
+            }
+            (x, y)
+        });
+    div_x * div_y
+}
+
+#[cfg(test)]
+mod tests {
+    use insta::assert_debug_snapshot;
+
+    use super::*;
+
+    #[test]
+    fn test_day_13_1() {
+        let result = day_13_1("./src/files/day13_1.test");
+        assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_day_13_2() {
+        let result = day_13_2("./src/files/day13_1.test");
+        assert_eq!(result, 140);
+    }
+
+    #[test]
+    fn test_packet_comparisons() {
+        let a = Packet::Int(1);
+        let b = Packet::Int(1);
+        let c = Packet::Int(2);
+        let d = Packet::Array(vec![Packet::Int(1), Packet::Int(2)]);
+        let e = Packet::Array(vec![Packet::Int(1), Packet::Int(2)]);
+        let f = Packet::Array(vec![Packet::Int(1), Packet::Int(3)]);
+        let g = Packet::Array(vec![Packet::Int(1), Packet::Int(2), Packet::Int(3)]);
+        let h = Packet::Array(vec![Packet::Int(1), Packet::Int(2), Packet::Int(3)]);
+        let i = Packet::Array(vec![Packet::Int(1)]);
+        let j = Packet::Array(vec![
+            Packet::Array(vec![]),
+            Packet::Int(2),
+            Packet::Int(3),
+            Packet::Int(4),
+        ]);
+
+        assert_eq!(a, b);
+        assert_ne!(a, c);
+        assert_ne!(a, d);
+        assert_eq!(d, e);
+        assert_ne!(d, f);
+        assert_ne!(d, g);
+        assert_eq!(g, h);
+        assert_eq!(a, i);
+        assert_ne!(i, j);
+
+        assert_eq!(a.partial_cmp(&b), Some(Ordering::Equal));
+        assert_eq!(a.partial_cmp(&c), Some(Ordering::Less));
+        assert_eq!(c.partial_cmp(&a), Some(Ordering::Greater));
+        assert_eq!(d.partial_cmp(&e), Some(Ordering::Equal));
+        assert_eq!(d.partial_cmp(&f), Some(Ordering::Less));
+        assert_eq!(f.partial_cmp(&d), Some(Ordering::Greater));
+        assert_eq!(a.partial_cmp(&h), Some(Ordering::Less));
+        assert_eq!(i.partial_cmp(&a), Some(Ordering::Equal));
+        assert_eq!(i.partial_cmp(&j), Some(Ordering::Greater));
+    }
+
+    #[test]
+    fn test_parse_line() {
+        let lines = vec![
+            "[1,2,3]".to_string(),
+            "[1,2,3,[4,5,[]]]".to_string(),
+            "[9]".to_string(),
+            "[[1],[2,3,4]]".to_string(),
+            "[[1],[2,3,14]]".to_string(),
+        ];
+        assert_debug_snapshot!(lines
+            .iter()
+            .map(|line| parse_line(line.to_string()))
+            .collect::<Vec<Vec<Packet>>>());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,133 @@
 #![feature(slice_as_chunks)]
+#![feature(is_sorted)]
+#![feature(let_chains)]
 
+use std::env;
 mod exercises;
 mod parser;
 
 fn main() {
-    // Day 1.1
-    let day1_1 = exercises::day1::day_1_1("./src/files/day1.1");
-    println!("Day 1.1 : {day1_1}");
+    if let Some(args) = env::args().nth(1) && args == "all" {
+        // Day 1.1
+        let day1_1 = exercises::day1::day_1_1("./src/files/day1.1");
+        println!("Day 1.1 : {day1_1}");
 
-    // Day 1.2
-    let day1_2 = exercises::day1::day_1_2("./src/files/day1.1");
-    println!("Day 1.2 : {day1_2}");
+        // Day 1.2
+        let day1_2 = exercises::day1::day_1_2("./src/files/day1.1");
+        println!("Day 1.2 : {day1_2}");
 
-    // Day 2.1
-    let day2_1 = exercises::day2::day_2_1("./src/files/day2");
-    println!("Day 2.1 : {day2_1}");
+        // Day 2.1
+        let day2_1 = exercises::day2::day_2_1("./src/files/day2");
+        println!("Day 2.1 : {day2_1}");
 
-    // Day 2.2
-    let day2_2 = exercises::day2::day_2_2("./src/files/day2");
-    println!("Day 2.2 : {day2_2}");
+        // Day 2.2
+        let day2_2 = exercises::day2::day_2_2("./src/files/day2");
+        println!("Day 2.2 : {day2_2}");
 
-    // Day 3.1
-    let day3_1 = exercises::day3::day_3_1("./src/files/day3");
-    println!("Day 3.1: {day3_1}");
+        // Day 3.1
+        let day3_1 = exercises::day3::day_3_1("./src/files/day3");
+        println!("Day 3.1: {day3_1}");
 
-    // Day 3.2
-    let day3_2 = exercises::day3::day_3_2("./src/files/day3");
-    println!("Day 3.2: {day3_2}");
+        // Day 3.2
+        let day3_2 = exercises::day3::day_3_2("./src/files/day3");
+        println!("Day 3.2: {day3_2}");
 
-    // Day 4.1
-    let day4_1 = exercises::day4::day_4_1("./src/files/day4");
-    println!("Day 4.1: {day4_1}");
+        // Day 4.1
+        let day4_1 = exercises::day4::day_4_1("./src/files/day4");
+        println!("Day 4.1: {day4_1}");
 
-    // Day 4.2
-    let day4_2 = exercises::day4::day_4_2("./src/files/day4");
-    println!("Day 4.2: {day4_2}");
+        // Day 4.2
+        let day4_2 = exercises::day4::day_4_2("./src/files/day4");
+        println!("Day 4.2: {day4_2}");
 
-    // Day 5.1
-    let day5_1 = exercises::day5::day_5_1("./src/files/day5");
-    print!("Day 5.1: ");
-    for c in day5_1 {
-        print!("{c}");
-    }
-    println!();
-
-    // Day 5.2
-    let day5_2 = exercises::day5::day_5_2("./src/files/day5");
-    print!("Day 5.2: ");
-    for c in day5_2 {
-        print!("{c}");
-    }
-    println!();
-
-    // Day 6.1
-    let day6_1 = exercises::day6::day_6_1("./src/files/day6");
-    println!("Day 6.1: {}", day6_1.first().unwrap());
-
-    // Day 6.2
-    let day6_2 = exercises::day6::day_6_2("./src/files/day6");
-    println!("Day 6.2: {}", day6_2.first().unwrap());
-
-    // Day 7.1
-    let day7_1 = exercises::day7::day_7_1("./src/files/day7");
-    println!("Day 7.1: {day7_1}");
-
-    // Day 7.2
-    let day7_2 = exercises::day7::day_7_2("./src/files/day7");
-    println!("Day 7.2: {day7_2}");
-
-    // Day 8.1
-    let day8_1 = exercises::day8::day_8_1("./src/files/day8", 99);
-    println!("Day 8.1: {day8_1}");
-
-    // Day 8.2
-    let day8_2 = exercises::day8::day_8_2("./src/files/day8", 99);
-    println!("Day 8.2: {day8_2}");
-
-    // Day 9.1
-    let day9_1 = exercises::day9::day_9_1("./src/files/day9");
-    println!("Day 9.1: {day9_1}");
-
-    // Day 9.2
-    let day9_2 = exercises::day9::day_9_2("./src/files/day9", 10);
-    println!("Day 9.2: {day9_2}");
-
-    // Day 10.1
-    let day10_1 = exercises::day10::day_10_1("./src/files/day10");
-    println!("Day 10.1: {day10_1}");
-
-    // Day 10.2
-    let day10_2 = exercises::day10::day_10_2("./src/files/day10");
-    println!("Day 10.2:");
-    for i in day10_2 {
-        for j in i {
-            if j {
-                print!("#")
-            } else {
-                print!(".")
-            }
+        // Day 5.1
+        let day5_1 = exercises::day5::day_5_1("./src/files/day5");
+        print!("Day 5.1: ");
+        for c in day5_1 {
+            print!("{c}");
         }
         println!();
+
+        // Day 5.2
+        let day5_2 = exercises::day5::day_5_2("./src/files/day5");
+        print!("Day 5.2: ");
+        for c in day5_2 {
+            print!("{c}");
+        }
+        println!();
+
+        // Day 6.1
+        let day6_1 = exercises::day6::day_6_1("./src/files/day6");
+        println!("Day 6.1: {}", day6_1.first().unwrap());
+
+        // Day 6.2
+        let day6_2 = exercises::day6::day_6_2("./src/files/day6");
+        println!("Day 6.2: {}", day6_2.first().unwrap());
+
+        // Day 7.1
+        let day7_1 = exercises::day7::day_7_1("./src/files/day7");
+        println!("Day 7.1: {day7_1}");
+
+        // Day 7.2
+        let day7_2 = exercises::day7::day_7_2("./src/files/day7");
+        println!("Day 7.2: {day7_2}");
+
+        // Day 8.1
+        let day8_1 = exercises::day8::day_8_1("./src/files/day8", 99);
+        println!("Day 8.1: {day8_1}");
+
+        // Day 8.2
+        let day8_2 = exercises::day8::day_8_2("./src/files/day8", 99);
+        println!("Day 8.2: {day8_2}");
+
+        // Day 9.1
+        let day9_1 = exercises::day9::day_9_1("./src/files/day9");
+        println!("Day 9.1: {day9_1}");
+
+        // Day 9.2
+        let day9_2 = exercises::day9::day_9_2("./src/files/day9", 10);
+        println!("Day 9.2: {day9_2}");
+
+        // Day 10.1
+        let day10_1 = exercises::day10::day_10_1("./src/files/day10");
+        println!("Day 10.1: {day10_1}");
+
+        // Day 10.2
+        let day10_2 = exercises::day10::day_10_2("./src/files/day10");
+        println!("Day 10.2:");
+        for i in day10_2 {
+            for j in i {
+                if j {
+                    print!("#")
+                } else {
+                    print!(".")
+                }
+            }
+            println!();
+        }
+        println!();
+
+        // Day 11.1
+        let day11_1 = exercises::day11::day_11_1("./src/files/day11");
+        println!("Day 11.1: {day11_1}");
+
+        // Day 11.2
+        let day11_2 = exercises::day11::day_11_2("./src/files/day11");
+        println!("Day 11.2: {day11_2}");
+
+        // Day 12.1
+        let day12_1 = exercises::day12::day_12_1("./src/files/day12") - 2;
+        println!("Day 12.1: {day12_1}");
+
+        // Day 12.2
+        let day12_2 = exercises::day12::day_12_2("./src/files/day12") - 2;
+        println!("Day 12.2: {day12_2}");
     }
-    println!();
+    // Day 13.1
+    let day13_1 = exercises::day13::day_13_1("./src/files/day13");
+    println!("Day 13.1: {day13_1}");
 
-    // Day 11.1
-    let day11_1 = exercises::day11::day_11_1("./src/files/day11");
-    println!("Day 11.1: {day11_1}");
-
-    // Day 11.2
-    let day11_2 = exercises::day11::day_11_2("./src/files/day11");
-    println!("Day 11.2: {day11_2}");
-
-    // Day 12.1
-    let day12_1 = exercises::day12::day_12_1("./src/files/day12") - 2;
-    println!("Day 12.1: {day12_1}");
-
-    // Day 12.2
-    let day12_2 = exercises::day12::day_12_2("./src/files/day12") - 2;
-    println!("Day 12.2: {day12_2}");
+    // Day 13.2
+    let day13_2 = exercises::day13::day_13_2("./src/files/day13");
+    println!("Day 13.2: {day13_2}");
 }
